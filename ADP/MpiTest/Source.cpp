510ef891e62565784f30b0d3431c2af9d45abe66
MONKEYDALYN\a <gogoasealin@gmail.com>
2018-04-13 12:26:18 +0300

    Tema 5 Cel mai mare numar Mpi

--- ADP/MpiTest/Source.cpp
+++ ADP/MpiTest/Source.cpp
@@ -1,17 +1,12 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
-	int v[100];
-	int vectorToCalculate[10];
-	int suma = 0;
-	int count = 10;
-	int sumaPrimita;
-	int procesCount = 0;
-
-	for (int i = 0; i < 100; i++) {
-		v[i] = i+1;
-	}
+	int random;
+	bool lider = true;
+	int otherNumbers;
 
 	// Initialize the MPI environment
 	MPI_Init(NULL, NULL);
@@ -30,50 +25,26 @@ int main(int argc, char** argv) {
 	int name_len;
 	MPI_Get_processor_name(processor_name, &name_len);
 
-	if (world_rank == 0) {
-		int k;
-		int world_rank_count = 1;
-		for (int i = 1; i < world_size; i++) {
-			for (int j = 0; j < count; j++) {
-			    k = j + (10 * world_rank_count);
-				vectorToCalculate[j] = v[k];
-				//printf(" pe pozitia %d este %d \n", j, vectorToSend[j]);		
+	srand(time(NULL) + (world_rank * 63));
+	random = rand() % 50;
+
+	for (int i = 0; i < world_size; i++) {
+		if (i != world_rank) {
+			MPI_Send(&random, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+			MPI_Recv(&otherNumbers, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+			if (random < otherNumbers) {
+				lider = false;
 			}
-			world_rank_count++;
-			MPI_Send(&vectorToCalculate, count, MPI_INT, i, 0, MPI_COMM_WORLD);
 		}
-
-		for (int j = 0; j < count; j++) {
-			vectorToCalculate[j] = v[j];
-		}
-
-		//for (int i = 0; i < count; i++) {
-		//	printf(" pe pozitia %d este %d \n", i, vectorToCalculate[i]);
-		//}
-
-		for (int i = 0; i < count; i++) {
-			suma = suma + vectorToCalculate[i];
-			//printf("Suma actuala %d \n", suma);
-		}
-		printf("suma trimisa de %d este %d ", world_rank, suma);
-		for (int i = 1; i < 4; i++) {
-			MPI_Recv(&sumaPrimita, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-			suma += sumaPrimita;
-		}
-		printf("Suma totala este %d \n", suma);
 	}
-	else {
-		MPI_Recv(&vectorToCalculate, count, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		for (int i = 0; i < count; i++) {
-			printf(" pe pozitia %d este %d \n", i, vectorToCalculate[i]);
-		}
-		for (int i = 0; i < count; i++) {
-			suma = suma + vectorToCalculate[i];
-		//	printf("Suma actuala %d \n", suma);
-		}
-		printf("suma trimisa de %d este %d ", world_rank, suma);
-		MPI_Send(&suma, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-		printf("process %d, has end ", world_rank);
+
+	printf("Hello world numarul : %d , rank %d"
+		" out of %d processors\n",
+		random , world_rank, world_size);
+
+	if (lider) {
+		printf("Cel mai mare numbar este %d al procesului %d ", random, world_rank);
 	}
 
 
